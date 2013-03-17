a518129c3d2fa031a402caaddec85129ea9cbc47
Sam Nayerman <sambackpack@gmail.com>
2013-03-16 19:25:00 -0700

    ADDED PAINT NIGG

--- src/Main.java
+++ src/Main.java
@@ -21,12 +21,14 @@ import java.awt.*;
 @Manifest(name = "Chitin Smither", authors = "OrangeJuice", description = "Smiths chitin to your heart's content!", version = 1.0)
 public class Main extends ActiveScript implements PaintListener {
 
-    public static int xpGained;
     private int startXP = Skills.getExperience(Skills.SMITHING);
     private long startTime;
+    private final Color color1 = new Color(239, 50, 50, 114);
+    private final Color color2 = new Color(0, 0, 0);
     public static Tile bTile = new Tile(3189, 3435, 0);
     public static Tile aTile = new Tile(3187, 3426, 0);
     public static SceneObject bank;
+    private final Font font1 = new Font("Arial", 0, 15);
     private Tree script = new Tree(new Node[] {
             new Banker(),
             new Smither()
@@ -51,22 +53,30 @@ public class Main extends ActiveScript implements PaintListener {
     }
 
     @Override
-    public void onRepaint(Graphics g) {
-        xpGained = Skills.getExperience(Skills.SMITHING)-startXP;
-        g.setColor(Color.GREEN);
-        g.fillRect(100, 450, 600, 80);
-        g.setColor(Color.BLACK);
+    public void onRepaint(Graphics g1) {
+        int xpGained = Skills.getExperience(Skills.SMITHING)-startXP;
+        Graphics2D g = (Graphics2D)g1;
+        g.setColor(color1);
+        g.fillRoundRect(546, 260, 199, 256, 16, 16);
+        g.setFont(font1);
+        g.setColor(color2);
+
         long millis = System.currentTimeMillis() - startTime;
         long hours = millis / (1000 * 60 * 60);
         millis -= hours * (1000 * 60 * 60);
         long minutes = millis / (1000 * 60);
         millis -= minutes * (1000 * 60);
         long seconds = millis / 1000;
-        g.drawString("Time: "+ hours + ": " + minutes + ": " + seconds, 120, 470);
-        g.drawString("Chitin/hr: "+(xpGained/10), 120, 490);
+
+        g.drawString("Chitin/Hr: "+(xpGained/10), 550, 339);
+        g.drawString("Time: "+ hours + ":" + minutes + ":" + seconds, 550, 295);
+        g.drawString("XP/Hr: " + getPerHour(xpGained), 550, 382);
+        g.drawString("XP Gained: " + xpGained, 550, 423);
     }
 
-
+    private int getPerHour(final long value) {
+        return (int) ((value) * 3600000D / (System.currentTimeMillis() - startTime));
+    }
 
     public class Banker extends Node {
 
